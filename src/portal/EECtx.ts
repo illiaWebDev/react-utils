d47189c2e32a6f0e90c3c05d1b55bff0a0cf5128
Illia <is@rost.company>
2023-02-23 20:52:33 +0200

    add replace props and more forgiving typings for portal Props

--- src/portal/EECtx.ts
+++ src/portal/EECtx.ts
@@ -5,7 +5,8 @@ import type { SafeOmit } from '@illia-web-dev/types/dist/types/Omit';
 import type { Reducer } from '../EECtx/store';
 
 
-export type Props = Record< string, unknown >;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Props = Record< string, any >;
 export type WithProps = {
   props: Props[];
 };
@@ -24,6 +25,7 @@ export const aTypes = {
   popAllPortals: '__illia-web-dev__/react-utils/portal/pop-all-portals',
   pushProps: '__illia-web-dev__/react-utils/portal/push-props',
   popProps: '__illia-web-dev__/react-utils/portal/pop-props',
+  replaceProps: '__illia-web-dev__/react-utils/portal/replace-props',
 } as const;
 
 
@@ -38,6 +40,10 @@ export type Actions = {
   popAllPortals: { type: typeof aTypes.popAllPortals };
   pushProps: { type: typeof aTypes.pushProps; payload: Props };
   popProps: { type: typeof aTypes.popProps };
+  replaceProps: {
+    type: typeof aTypes.replaceProps;
+    payload: Props;
+  };
 };
 export type AllActions = RecordValues< Actions >;
 
@@ -48,6 +54,7 @@ export const aCreators: ACreators< Actions > = {
   popAllPortals: () => ( { type: aTypes.popAllPortals } ),
   pushProps: p => ( { type: aTypes.pushProps, payload: p } ),
   popProps: () => ( { type: aTypes.popProps } ),
+  replaceProps: p => ( { type: aTypes.replaceProps, payload: p } ),
 };
 
 
@@ -88,6 +95,27 @@ export const reducer: Reducer< State, AllActions > = ( { s = defaultState, actio
         ...s,
         portals: s.portals.map(
           ( it, i ) => ( i === lastPortalIndx ? { ...it, props: it.props.slice( 0, -1 ) } : it ),
+        ),
+      };
+    }
+    case aTypes.replaceProps: {
+      const lastPortalIndx = s.portals.length - 1;
+      const lastPortal = s.portals[ lastPortalIndx ];
+      if ( lastPortal === undefined ) return s;
+
+      return {
+        ...s,
+        portals: s.portals.map(
+          it => {
+            if ( it !== lastPortal ) return it;
+
+            const propsLength = it.props.length;
+            const nextProps = propsLength === 0 ? [action.payload] : (
+              it.props.slice( 0, -1 ).concat( action.payload )
+            );
+
+            return { ...it, props: nextProps };
+          },
         ),
       };
     }
