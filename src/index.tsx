cc0e85270897e59893970017d93c1706091ea520
Illia <is@rost.company>
2023-03-05 15:34:36 +0200

    useStateRef

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export * as EECtxNS from './EECtx';
 export * as PortalNS from './portal';
+export * as utils from './utils';
 // export * as PopperNS from './popper';
---
+++ src/utils/useStateRef.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const useStateRef = < T >(
+  initialValue: T,
+  compare: ( a: T, b: T ) => boolean = ( a, b ) => a === b,
+): [T, React.Dispatch<React.SetStateAction<T>>, React.MutableRefObject< T >] => {
+  const [value, setValue] = React.useState( initialValue );
+  const valueRef = React.useRef( value );
+  if ( compare( valueRef.current, value ) === false ) valueRef.current = value;
+
+  return React.useMemo( () => [
+    value,
+    setValue,
+    valueRef,
+  ], [value] );
+};
